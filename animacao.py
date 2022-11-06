8de0f6fad05b337647ab829bbe084b61b10e2fe1
FabricioVenturim <fabriciodalviventurim@gmail.com>
2022-11-06 15:58:42 -0300

    Adiciona player 2

--- animacao.py
+++ animacao.py
@@ -11,13 +11,15 @@ h = 648
 screen = pygame.display.set_mode((w, h))
 game = True
 
-### TESTE ###
+dict_animacoes_boy = {"parado": [0, 232, 455], "correndo": [5940, 363, 455], "pulando": [2325, 362, 483]}
+sprites_boy = pygame.sprite.Group()
+boy = personagem.BoyNinja(500, 500, "img/spritesheet_boy.png", dict_animacoes_boy)
+sprites_boy.add(boy)
 
-todas_as_sprites = pygame.sprite.Group()
-girl = personagem.Personagem(500, 500)
-todas_as_sprites.add(girl)
-
-########################
+dict_animacoes_girl = {"parado": [0, 290, 500], "correndo": [6906, 372, 500], "pulando": [2910, 399, 500]}
+sprites_girl = pygame.sprite.Group()
+girl = personagem.GirlNinja(100, 500, "img/spritesheet_girl.png", dict_animacoes_girl)
+sprites_girl.add(girl)
 
 relogio = pygame.time.Clock()
 
@@ -30,22 +32,37 @@ while game:
     
     #Eventos de segurar a tecla
     if pygame.key.get_pressed()[K_d]:
+        boy.correr_direita()
+
+    if pygame.key.get_pressed()[K_RIGHT]:
         girl.correr_direita()
 
     if pygame.key.get_pressed()[K_a]:
-        girl.correr_esquerda() 
+        boy.correr_esquerda() 
+
+    if pygame.key.get_pressed()[K_LEFT]:
+        girl.correr_esquerda()
 
     #Evetos sem segurar a tecla
     for event in pygame.event.get():
         if event.type == QUIT:
             game = False
         if event.type == pygame.KEYDOWN:
-            if event.key == K_w and girl.pular == False:
+            if event.key == K_w and boy.pular == False:
+                boy.fun_pular()
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_UP and girl.pular == False:
                 girl.fun_pular()
-        
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == K_f:
+                boy.fun_bater()
     
 
-    todas_as_sprites.draw(screen)
-    todas_as_sprites.update()
+    sprites_boy.draw(screen)
+    sprites_boy.update()
+    sprites_girl.draw(screen)
+    sprites_girl.update()
+
     pygame.display.flip()
 
--- personagem.py
+++ personagem.py
@@ -13,37 +13,38 @@ gravidade = 6
 velo_pulo = 50
 
 class Personagem(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, img, dict_animacoes):
+        pygame.sprite.Sprite.__init__(self)
         self.x = x
         self.y = y
         self.chao = 425
         self.speedy = velo_pulo
         self.state = 0
 
-        sprite_sheet = pygame.image.load("img/spritesheet.png").convert_alpha()
-        pygame.sprite.Sprite.__init__(self)
+        sprite_sheet = pygame.image.load(img).convert_alpha()
         self.imagens_ninja = []
+        
         # parado
+    
         for i in range(0, 10):
-            largura_inicial = 10180
-            img = sprite_sheet.subsurface((largura_inicial + i*232,0), (232,455))
-            img = pygame.transform.scale(img, (232/3, 455/3))
+            largura_inicial = dict_animacoes["parado"][0]
+            img = sprite_sheet.subsurface((largura_inicial + i*dict_animacoes["parado"][1],0), (dict_animacoes["parado"][1],dict_animacoes["parado"][2]))
+            img = pygame.transform.scale(img, (dict_animacoes["parado"][1]/3, dict_animacoes["parado"][2]/3))
             self.imagens_ninja.append(img)
 
         # correndo
         for i in range(0,10):
-            largura_inicial = 21160
-            img = sprite_sheet.subsurface((largura_inicial + i*363,0), (363,455))
-            img = pygame.transform.scale(img, (363/3, 455/3))
-            self.imagens_ninja.append(img) # 9 a 18
+            largura_inicial = dict_animacoes["correndo"][0]
+            img = sprite_sheet.subsurface((largura_inicial + i*dict_animacoes["correndo"][1],0), (dict_animacoes["correndo"][1],dict_animacoes["correndo"][2]))
+            img = pygame.transform.scale(img, (dict_animacoes["correndo"][1]/3, dict_animacoes["correndo"][2]/3))
+            self.imagens_ninja.append(img) 
 
         # pulando
         for i in range(0,10): 
-            largura_inicial = 12510
-            img = sprite_sheet.subsurface((largura_inicial + i*362,0), (362,483))
-            img = pygame.transform.scale(img, (362/3, 483/3))
-            self.imagens_ninja.append(img) # 9 a 18
-
+            largura_inicial = dict_animacoes["pulando"][0]
+            img = sprite_sheet.subsurface((largura_inicial + i*dict_animacoes["pulando"][1],0), (dict_animacoes["pulando"][1],dict_animacoes["correndo"][2]))
+            img = pygame.transform.scale(img, (dict_animacoes["pulando"][1]/3, dict_animacoes["pulando"][2]/3))
+            self.imagens_ninja.append(img) 
 
         self.index_lista = 0
         self.image = self.imagens_ninja[self.index_lista]
@@ -53,7 +54,8 @@ class Personagem(pygame.sprite.Sprite):
         self.direita = True
         self.correr = False
         self.pular = False
-    
+        self.bater = False
+
     def correr_direita(self): 
         self.direita = True
         self.correr = True
@@ -78,11 +80,13 @@ class Personagem(pygame.sprite.Sprite):
         self.pular = True
         self.correr = False
         self.state = 1
-        if self.index_lista < 19:
-            self.index_lista = 19
+        if self.index_lista < 20:
+            self.index_lista = 20
 
-  
     def update(self):
+        #############
+        ## Correr ###
+        #############
         if self.correr and self.pular == False:
             if self.index_lista > 19:   
                 self.index_lista = 10
@@ -94,11 +98,14 @@ class Personagem(pygame.sprite.Sprite):
                 self.image = pygame.transform.flip(self.image, True, False)
             self.correr = False
 
+        #############
+        ### Pular ###
+        #############
         elif self.pular:
             
             ####### animação #######
-            if self.index_lista > 28:   
-                self.index_lista = 19
+            if self.index_lista > 29:   
+                self.index_lista = 20
             self.index_lista += 0.5
             self.image = self.imagens_ninja[int(self.index_lista)]
             
@@ -127,6 +134,7 @@ class Personagem(pygame.sprite.Sprite):
                 # Atualiza o estado para parado
                 self.pular = False
                 self.state = 0
+
         else: 
             if self.index_lista > 9:
                 self.index_lista = 0
@@ -135,4 +143,39 @@ class Personagem(pygame.sprite.Sprite):
             # vira a imagem se o personagem estiver olhando para o outro lado
             if self.direita == False:
                 self.image = pygame.transform.flip(self.image, True, False)
+
+class BoyNinja(Personagem):
+    def __init__(self, x, y, img, dict_animacoes):
+        super().__init__(x, y, img, dict_animacoes)
+
+    # #batendo
+    #     for i in range(0,10): 
+    #         largura_inicial = 0
+    #         img = sprite_sheet.subsurface((largura_inicial + i*536,0), (536,495))
+    #         img = pygame.transform.scale(img, (536/3, 495/3))
+    #         self.imagens_ninja.append(img) 
     
+    def fun_bater(self):
+        self.bater = True
+        if self.index_lista < 30:
+            self.index_lista = 30
+        
+    # def update(self):
+    #     #############
+    #     ### Bater ###
+    #     #############
+    #     if self.bater:
+    #         if self.index_lista > 39:   
+    #             self.index_lista = 30
+    #             self.bater = False
+    #         self.index_lista += 0.5
+    #         self.image = self.imagens_ninja[int(self.index_lista)]
+            
+    #         # vira a imagem se o personagem estiver olhando para o outro lado
+    #         if self.direita == False:
+    #             self.image = pygame.transform.flip(self.image, True, False)
+
+
+class GirlNinja(Personagem):
+    def __init__(self, x, y, img, dict_animacoes):
+        super().__init__(x, y, img, dict_animacoes)
