0a885f54eea7926f5b787ca9c8a31342c102ad16
Lhc128 <lucashcc8@gmail.com>
2022-11-19 02:40:02 -0300

    comece a fazer a interface

--- sound effects.py
+++ sound effects.py
@@ -1,18 +1,93 @@
-import pygame
+import pygame, sys
 from time import sleep
-from pygame import mixer 
+from pygame import mixer
+from pygame.locals import *
 
 #para começar o pygame
 pygame.init()
 
 #criando a tela do jogo
-tela = pygame.display.set_mode((1920, 1080))
-fundo = pygame.image.load("fundo.jpg")
+tela = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
 tela.fill((0,0,0))
-tela.blit(fundo, (0, 0))
+
+#criando título e ícone
+pygame.display.set_caption("Double Ninjas: Uma Missão Quase Possível")
+icon = pygame.image.load("ying yang.png")
+pygame.display.set_icon(icon)
 
 #musica de fundo
 mixer.music.load('trilha sonora.mp3')
 mixer.music.play()
 
-sleep(10)
+fonte = pygame.font.SysFont(None, 20)
+
+def texto(texto, fonte, cor, superficie, x, y):
+    textobj = fonte.render(texto, 1, cor)
+    textoret = textobj.get_rect()
+    superficie.blit(textobj, textoret)
+
+def menu():
+    while True:
+        tela.fill((0,0,0))
+        texto("Menu", fonte, (255,255,255), tela, 20, 20)
+
+        mx, my = pygame.mouse.get_pos()
+
+        botao1 = pygame.Rect(50, 100, 200, 50)
+        botao2 = pygame.Rect(50, 200, 200, 50)
+        if botao1.collidepoint((mx, my)):
+            if click:
+                jogo()
+        if botao2.collidepoint((mx, my)):
+            if click:
+                opcoes()
+
+        pygame.draw.rect(tela, (255,0,0), botao1)
+        pygame.draw.rect(tela, (255,0,0), botao2)
+
+        for evento in pygame.event.get():
+            if evento.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if evento.type == KEYDOWN:
+                if evento.key == K_ESCAPE:
+                    pygame.quit()
+                    sys.exit
+            if evento.type == MOUSEBUTTONDOWN:
+                if evento.button == 1:
+                    click = True
+        
+        pygame.display.update()
+        pygame.time.Clock().tick(60)
+
+def jogo():
+    rodar = True
+    while rodar:
+        texto("Jogar", fonte, (255,255,255), tela, 20, 20)
+        for evento in pygame.event.get():
+            if evento.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if evento.type == KEYDOWN:
+                if evento.key == K_ESCAPE:
+                    rodar = False
+        
+        pygame.display.update()
+        pygame.time.Clock().tick(60)
+
+def opcoes():
+    rodar = True
+    while rodar:
+        texto("Opções", fonte, (255,255,255), tela, 20, 20)
+        for evento in pygame.event.get():
+            if evento.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            if evento.type == KEYDOWN:
+                if evento.key == K_ESCAPE:
+                    rodar = False
+        pygame.display.update()
+        pygame.time.Clock().tick(60)
+
+menu()
+
