1110789a7cfddd69d648040e861274e0702dc0e7
Lhc128 <lucashcc8@gmail.com>
2022-11-26 12:54:04 -0300

    termine a o design inicial da interface

--- interface.py
+++ interface.py
@@ -1,95 +1,91 @@
-import pygame, sys
-from time import sleep
-from pygame import mixer
+import pygame
 from pygame.locals import *
 
-#para começar o pygame
-pygame.init()
+class Interface:
+    def __init__(self):
+        pygame.init()
+        pygame.mixer.init()
+        pygame.font.init()
+        pygame.display.set_caption("Double Ninjas: Uma Missão Quase Possível")
+        icon = pygame.image.load("img/ying yang.png")
+        pygame.display.set_icon(icon)
+        self.tela = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
+        self.fonte = pygame.font.SysFont("Arial", 36)
+        self.tempo = pygame.time.Clock()
+        self.rodando = True
+        fundo = pygame.image.load("img/fundo2.jpg")
+        fundo = pygame.transform.scale(fundo,(1920,1080))
+        self.tela.blit(fundo,(0,0))
 
-#criando a tela do jogo
-tela = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-tela.fill((0,0,0))
+    def novo_jogo(self):
+        self.todas_sprites = pygame.sprite.Group()
+        self.rodar()
 
-#criando título e ícone
-pygame.display.set_caption("Double Ninjas: Uma Missão Quase Possível")
-icon = pygame.image.load("ying yang.png")
-pygame.display.set_icon(icon)
-
-#musica de fundo
-mixer.music.load('trilha sonora.mp3')
-mixer.music.play()
-
-fonte = pygame.font.SysFont(None, 20)
-
-def texto(texto, fonte, cor, superficie, x, y):
-    textobj = fonte.render(texto, 1, cor)
-    textoret = textobj.get_rect()
-    superficie.blit(textobj, textoret)
-
-
-
-def jogo():
-    rodar = True
-    while rodar:
-        texto("Jogar", fonte, (255,255,255), tela, 50, 50)
+    def rodar(self):
+        self.jogando = True
+        while self.jogando:
+            self.tempo.tick(60)
+            self.eventos()
+    
+    def eventos(self):
         for evento in pygame.event.get():
-            if evento.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            if evento.type == KEYDOWN:
-                if evento.key == K_ESCAPE:
-                    rodar = False
+            if evento.type == pygame.QUIT:
+                if self.jogando:
+                    self.jogando = False
+                self.rodando = False
+
+
+    def mostrar_texto(self, texto, tamanho, cor, x, y):
+        fonte = pygame.font.SysFont("Arial", tamanho)
+        texto = fonte.render(texto, True, cor)
+        texto_ret = texto.get_rect()
+        #para posicionar o texto de acordo com o centro
+        texto_ret.midtop = (x, y)
+        self.tela.blit(texto, texto_ret)
+
+    def tela_start(self):
+        pygame.mixer.music.load("audio/trilha sonora2.mp3")
+        pygame.mixer.music.play()
+        self.mostrar_texto(
+            "- Pressione uma tecla para jogar", 
+            45, 
+            #(94, 66, 39),
+            (255,255,255), 
+            self.tela.get_width()/2, 
+            self.tela.get_height()*4/5
+            )
+        self.mostrar_texto(
+            "- Desenvolvido por Fabrício Venturin, Lucas Cuan, Pedro Thomaz Martins e Yonathan Rabinovici", 
+            12, 
+            (255, 255, 255), 
+            self.tela.get_width()/2, 
+            self.tela.get_height()/100
+            )
         
-        pygame.display.update()
-        pygame.time.Clock().tick(60)
+        #para atualizar a tela enquanto aparece o texto
+        pygame.display.flip()
+        self.esperar()
 
-def opcoes():
-    rodar = True
-    while rodar:
-        texto("Opções", fonte, (255,255,255), tela, 50, 50)
-        for evento in pygame.event.get():
-            if evento.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            if evento.type == KEYDOWN:
-                if evento.key == K_ESCAPE:
-                    rodar = False
-        pygame.display.update()
-        pygame.time.Clock().tick(60)
-        
-def menu():
-    while True:
-        tela.fill((0,0,0))
-        texto("Menu", fonte, (255,255,255), tela, 50, 50)
+    def esperar(self):
+        esperando = True
+        while esperando:
+            self.tempo.tick(60)
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    esperando = False
+                    self.rodar = False
+                if evento.type == pygame.KEYUP:
+                    esperando = False
+                    pygame.mixer.music.stop()
+                    pygame.mixer.Sound("audio/trilha sonora.mp3").play()
 
-        mx, my = pygame.mouse.get_pos()
+    def tela_quit(self):
+        pygame.quit()
+        pass
 
-        botao1 = pygame.Rect(1500, 350, 200, 100)
-        botao2 = pygame.Rect(280, 350, 200, 100)
-        if botao1.collidepoint((mx, my)):
-            if click:
-                jogo()
-        if botao2.collidepoint((mx, my)):
-            if click:
-                opcoes()
+jogo = Interface()
+jogo.tela_start()
 
-        pygame.draw.rect(tela, (51,25,0), botao1)
-        pygame.draw.rect(tela, (51,25,0), botao2)
-
-        for evento in pygame.event.get():
-            if evento.type == QUIT:
-                pygame.quit()
-                sys.exit()
-            if evento.type == KEYDOWN:
-                if evento.key == K_ESCAPE:
-                    pygame.quit()
-                    sys.exit
-            if evento.type == MOUSEBUTTONDOWN:
-                if evento.button == 1:
-                    click = True
-        
-        pygame.display.update()
-        pygame.time.Clock().tick(60)
-
-menu()
-
+while jogo.rodar:
+    jogo.novo_jogo()
+    jogo.tela_quit()
