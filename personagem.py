2ef7f8e2b220db9b5f9c3900730715c2f21dfeff
FabricioVenturim <fabriciodalviventurim@gmail.com>
2022-11-28 20:03:39 -0300

    Adiciona docstring nas funções

--- personagem.py
+++ personagem.py
@@ -3,6 +3,18 @@ from pygame.locals import *
 
 
 class Personagem(pygame.sprite.Sprite):
+    """Objeto com funções básicas de todos os personagens
+
+    :param x: Posição x do personagem
+    :type x: int or float
+    :param y: Posição y do personagem
+    :type y: int or float
+    :param img: Caminho da imagem do personagem
+    :type img: str
+    :param dict_animacoes: Dicionário com as posições de cada animação
+    :type dict_animacoes: dict
+    """    
+
     # Define estados possíveis do jogador
     # parado = 0 
     # pulando = 1
@@ -13,7 +25,18 @@ class Personagem(pygame.sprite.Sprite):
     # Define a velocidade inicial no pulo
     aceleracao_pulo_inicial = 30
 
-    def __init__(self, x, y, img, dict_animacoes):
+    def __init__(self, x:int, y:int, img:str, dict_animacoes:dict):
+        """Funções construtoras do objeto
+
+        :param x: Posição x do personagem
+        :type x: int or float
+        :param y: Posição y do personagem
+        :type y: int or float
+        :param img: Caminho da imagem do personagem
+        :type img: str
+        :param dict_animacoes: Dicionário com as posições de cada animação
+        :type dict_animacoes: dict
+        """        
         pygame.sprite.Sprite.__init__(self)
         self.aceleracao = self.aceleracao_pulo_inicial
         self.__state = 2
@@ -82,7 +105,22 @@ class Personagem(pygame.sprite.Sprite):
     def planar(self, value):
         self.__planar = value
 
-    def corta_sprite(self,sprite_sheet, posicao_inicial, largura, altura, quantidade, redirecionamento):        
+    def corta_sprite(self,sprite_sheet:str, posicao_inicial:float, largura:float, altura:float, quantidade:int, redirecionamento:float):
+        """Função que corta a sprite sheet e adiciona as imagens em uma lista
+
+        :param sprite_sheet: Caminho da imagem do personagem
+        :type sprite_sheet: str
+        :param posicao_inicial: Posição inicial da sprite sheet
+        :type posicao_inicial: float
+        :param largura: Largura da imagem
+        :type largura: float
+        :param altura: Altura da imagem
+        :type altura: float
+        :param quantidade: Quantidade de imagens
+        :type quantidade: int
+        :param redirecionamento: Redirecionamento da imagem
+        :type redirecionamento: float
+        """                
         for i in range(0, quantidade):
             largura_inicial = posicao_inicial
             img = sprite_sheet.subsurface((largura_inicial + i*largura,0), (largura,altura))
@@ -90,6 +128,8 @@ class Personagem(pygame.sprite.Sprite):
             self.imagens_ninja.append(img)
     
     def parado_animacao(self):
+        """_summary_: Função que define a animação do personagem quando parado
+        """        
         if self.index_lista > 9:
             self.index_lista = 0
         self.index_lista += 0.25
@@ -100,6 +140,8 @@ class Personagem(pygame.sprite.Sprite):
 
 
     def fun_correr_direita(self): 
+        """_summary_: Função que muda a posição do personagem correndo para a direita
+        """        
         self.direita = True
         self.correr = True
         if self.index_lista < 10:
@@ -110,6 +152,8 @@ class Personagem(pygame.sprite.Sprite):
             self.rect.x += 6
     
     def fun_correr_esquerda(self):
+        """_summary_: Função que muda a posição do personagem correndo para a esquerda
+        """        
         self.direita = False
         self.correr = True
         if self.index_lista < 10:
@@ -120,6 +164,8 @@ class Personagem(pygame.sprite.Sprite):
             self.rect.x -= 6
 
     def correr_animacao(self):
+        """_summary_: Função que define a animação do personagem quando correndo
+        """        
         if self.index_lista > 19:   
             self.index_lista = 10
         self.index_lista += 0.25
@@ -131,11 +177,15 @@ class Personagem(pygame.sprite.Sprite):
         self.correr = False
         
     def fun_cair(self):
+        """_summary_: Função que muda a posição do personagem caindo
+        """        
         self.correr = False
         self.planar = False
         self.state = 2
 
     def cair(self):
+        """_summary_: Função que define a animação do personagem quando caindo
+        """        
         self.rect.y += self.aceleracao
         self.aceleracao += self.gravidade
         self.index_lista = 24
@@ -148,12 +198,16 @@ class Personagem(pygame.sprite.Sprite):
             self.aceleracao = 18
 
     def fun_pular(self):
+        """_summary_: Função que muda a posição do personagem pulando
+        """        
         self.correr = False
         self.state = 1
         if self.index_lista < 20:
             self.index_lista = 20
 
     def pular_animacao(self):
+        """_summary_: Função que define a animação do personagem quando pulando
+        """        
         ####### animação #######
         if self.index_lista > 29:   
             self.index_lista = 20
@@ -179,7 +233,29 @@ class Personagem(pygame.sprite.Sprite):
             
 
 class BoyNinja(Personagem):
+    """_summary_: Classe que define o personagem ninja menino
+
+    :param x: Posição x do personagem
+    :type x: int or float
+    :param y: Posição y do personagem
+    :type y: int or float
+    :param img: Caminho da imagem do personagem
+    :type img: str
+    :param dict_animacoes: Dicionário com as posições de cada animação
+    :type dict_animacoes: dict
+    """    
     def __init__(self, x, y, img, dict_animacoes):
+        """_summary_: Construtor da classe
+
+        :param x: Posição x do personagem
+        :type x: int or float
+        :param y: Posição y do personagem
+        :type y: int or float
+        :param img: Caminho da imagem do personagem
+        :type img: str
+        :param dict_animacoes: Dicionário com as posições de cada animação
+        :type dict_animacoes: dict
+        """        
         super().__init__(x, y, img, dict_animacoes)
         self.__bater = False
 
@@ -192,12 +268,16 @@ class BoyNinja(Personagem):
         self.__bater = value
 
     def fun_bater(self):
+        """_summary_: Função que ativa o personagem batendo
+        """        
         self.bater = True
         self.correr = False
         if self.index_lista < 30:
             self.index_lista = 30
 
     def bater_animacao(self):
+        """_summary_: Função que define a animação do personagem quando bate
+        """        
         #Deixar a animação mais suave
         if self.index_lista == 30:
             if self.direita:
@@ -222,6 +302,8 @@ class BoyNinja(Personagem):
        
 
     def fun_planar(self):
+        """_summary_: Função que ativa a planagem do personagem
+        """        
         self.correr = False
         self.planar = True
         self.state = 2
@@ -230,6 +312,8 @@ class BoyNinja(Personagem):
  
 
     def planar_animacao(self):
+        """_summary_: Função que define a animação do personagem quando plana
+        """        
         if self.index_lista > 49:   
             self.index_lista = 40
         self.index_lista += 0.5
@@ -245,6 +329,8 @@ class BoyNinja(Personagem):
     
 
     def update(self):
+        """_summary_: Função que atualiza a posição do personagem
+        """        
         if self.state == 0:
             self.aceleracao = self.aceleracao_pulo_inicial
             self.planar = False
@@ -268,7 +354,29 @@ class BoyNinja(Personagem):
             self.parado_animacao()
             
 class GirlNinja(Personagem):
+    """_summary_: Classe que define o personagem ninja menina
+
+        :param x: Posição x do personagem
+        :type x: int or float
+        :param y: Posição y do personagem
+        :type y: int or float
+        :param img: Caminho da imagem do personagem
+        :type img: str
+        :param dict_animacoes: Dicionário com as posições de cada animação
+        :type dict_animacoes: dict
+        """   
     def __init__(self, x, y, img, dict_animacoes, screen):
+        """_summary_: Construtor da classe
+
+        :param x: Posição x do personagem
+        :type x: int or float
+        :param y: Posição y do personagem
+        :type y: int or float
+        :param img: Caminho da imagem do personagem
+        :type img: str
+        :param dict_animacoes: Dicionário com as posições de cada animação
+        :type dict_animacoes: dict
+        """   
         super().__init__(x, y, img, dict_animacoes)
         self.screen = screen
         self.__deslizar = False
@@ -292,6 +400,8 @@ class GirlNinja(Personagem):
         self.__atirar = value
 
     def fun_deslizar(self):
+        """_summary_: Função que ativa o personagem deslizando
+        """        
         self.deslizar = True
         self.correr = False
         self.rect.y += 37
@@ -304,6 +414,8 @@ class GirlNinja(Personagem):
             self.rect.x += 7.5
 
     def deslizar_animacao(self):
+        """_summary_: Função que define a animação do personagem quando desliza
+        """        
         if self.index_lista > 39:   
             self.index_lista = 30
         self.index_lista += 0.25
@@ -316,6 +428,8 @@ class GirlNinja(Personagem):
         self.rect.y -= 37
 
     def fun_atirar(self):
+        """_summary_: Função que ativa o personagem atirando
+        """        
         self.atirar = True
         self.correr = False
 
@@ -323,6 +437,8 @@ class GirlNinja(Personagem):
             self.index_lista = 40
             
     def atirar_animacao(self):
+        """_summary_: Função que define a animação do personagem quando atira
+        """        
         if self.index_lista > 49:   
             self.index_lista = 40
             self.atirar = False
@@ -338,6 +454,8 @@ class GirlNinja(Personagem):
             self.image = pygame.transform.flip(self.image, True, False)
 
     def update(self):
+        """_summary_: Função que atualiza a posição do personagem
+        """        
         # Atualiza a posição do kunai
         self.kunai.update()
 
@@ -367,10 +485,29 @@ class GirlNinja(Personagem):
 
 
 class Robo(Personagem):
+    """_summary_: Classe que representa o Robo 
+
+        :param x: posição x do robo
+        :type x: int
+        :param x_distancia: distancia que o robo irá se mover
+        :type x_distancia: int   
+        :param y: posição y do robo
+        :type y: int
+        :param campo_de_visao: distancia que o robo irá ver o personagem
+        :type campo_de_visao: int
+        :param img: imagem do robo
+        :type img: str
+        :param dict_animacoes: dicionario com as animações do robo
+        :type dict_animacoes: dict
+        :param movimentacao: Se o robô irá se mover ou ficará parado, defaults to True
+        :type movimentacao: bool, optional
+        :param direita_movimentacao: Qual direção o robô irá se mover de acordo com a posição inicial, defaults to True
+        :type direita_movimentacao: bool, optional
+        """        
     temporizador = 0
 
-    def __init__(self, x, x_distancia, y, campo_de_visao, img, dict_animacoes, movimentacao = True, direita_movimentacao = True):
-        """_summary_: Classe que representa o Robo 
+    def __init__(self, x:int, x_distancia:int, y:int, campo_de_visao:int, img:str, dict_animacoes:dict, movimentacao: bool = True, direita_movimentacao: bool = True):
+        """_summary_: Construtor da classe Robo
 
         :param x: posição x do robo
         :type x: int
@@ -439,11 +576,15 @@ class Robo(Personagem):
         self.__vivo = value
     
     def fun_morrer(self):
+        """_summary_: Função que faz a animação do robo morrer
+        """        
         self.vivo = False
         if self.index_lista < 18:
             self.index_lista = 18
 
     def correr_animacao(self):
+        """_summary_: Função que faz a animação do robo correr
+        """        
         if self.index_lista > 17:   
             self.index_lista = 10
         self.index_lista += 0.25
@@ -454,7 +595,12 @@ class Robo(Personagem):
             self.image= pygame.transform.flip(self.image, True, False)
         self.correr = False
     
-    def verifica_player(self, player):
+    def verifica_player(self, player: object):
+        """_summary_: Função que verifica se o player está dentro do campo de visão do robo
+
+        :param player: Objeto do tipo Personagem
+        :type player: object
+        """        
         if self.direita:     # verifica se o player está no campo de visão x        # verifica se o player está no campo de visão y
             if self.rect.x < player.rect.x < self.rect.x + self.campo_de_visao and self.rect.y - 50 <= player.rect.y <= self.rect.y + 50:
                 self.correr = False
@@ -466,6 +612,8 @@ class Robo(Personagem):
                 print("ESQUERDA campo de visão") 
 
     def animacao_morrer(self):
+        """_summary_: Função que faz a animação do robo morrer
+        """        
         if self.index_lista == 24: # quando a colisão tiver certa, aí isso vai sair
             self.rect.y += 10
         if self.index_lista > 27:   
@@ -479,6 +627,8 @@ class Robo(Personagem):
         self.correr = False
 
     def update(self):
+        """_summary_: Função que atualiza a posição do robo
+        """        
         if self.state == 2:
             self.cair()
 
@@ -512,10 +662,20 @@ class Robo(Personagem):
         
 
 class Kunai(pygame.sprite.Sprite):
+    """_summary_ : Classe que cria os objetos Kunai
+
+    :param screen: tela do jogo que será exibido os objetos
+    :type screen: str
+    """    
     gravidade = 1.5
     aceleracao_inicial = 25
 
-    def __init__(self, screen):       
+    def __init__(self, screen:str):  
+        """_summary_: Função que inicializa os objetos Kunai
+
+        :param screen: tela do jogo que será exibido os objetos
+        :type screen: str
+        """             
         pygame.sprite.Sprite.__init__(self)
 
         self.screen = screen
@@ -566,7 +726,16 @@ class Kunai(pygame.sprite.Sprite):
     def atirar(self, value):
         self.__atirar = value
 
-    def fun_atirar(self, x, y, bool_direita):
+    def fun_atirar(self, x:int, y:int, bool_direita:bool):
+        """_summary_: Função que faz o objeto Kunai atirar
+
+        :param x: posição x do objeto
+        :type x: int
+        :param y: posição y do objeto
+        :type y: int
+        :param bool_direita: variável que verifica se o objeto está olhando para a direita
+        :type bool_direita: bool
+        """        
         self.atirar = True
         self.direita = bool_direita
         if self.direita:
@@ -575,6 +744,8 @@ class Kunai(pygame.sprite.Sprite):
             self.rect.midbottom = (x, y)
 
     def trajetoria(self):
+        """_summary_: Função que faz o objeto Kunai seguir uma trajetória
+        """        
         if self.direita:
             self.rect.x += 15
         else:
@@ -589,6 +760,8 @@ class Kunai(pygame.sprite.Sprite):
             self.screen.blit(pygame.transform.flip(self.image, True, False), self.rect)
 
     def update(self):
+        """_summary_: Função que atualiza a posição do objeto Kunai
+        """        
         if self.atirar:
             self.trajetoria()
         else:
