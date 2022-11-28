72dc5c6e6370b00868db9b4bc00ae4bad2709ed7
Pedro Thomaz Conzatti Martins <pedrothomazm@gmail.com>
2022-11-27 21:07:00 -0300

    Remova espaço entre Tiles

--- tile.py
+++ tile.py
@@ -1,11 +1,12 @@
 import pygame
-
+from math import ceil
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, size):
         super().__init__()
+        rounded_up = ceil(size)
         # Um bloco branco como placeholder
-        self.image = pygame.Surface((size, size))
+        self.image = pygame.Surface((rounded_up, rounded_up))
         self.image.fill((255, 255, 255))
         
         self.rect = self.image.get_rect(topleft=pos)
