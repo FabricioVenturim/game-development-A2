9cb52bbdb615271de1c9edd3dcc87fb42aed66c6
Lhc128 <lucashcc8@gmail.com>
2022-12-05 03:14:38 -0300

    crie as novas fases

--- config.py
+++ config.py
@@ -119,17 +119,17 @@ level_data = [
             'X    XXXX       XX',
             'X                X',
             'X     M        V X',
-            'X                X',
+            'X        C       X',
             'X       XXX      X',
             'X   V            X',
             'X                X',
-            'X B    C   G     X',
+            'X B        G     X',
             'XXXXXXXXXXXXXXXXXX'
         ], 'sprite_settings': {
             'R': [
                 {
                     'x_distancia': 2,
-                    'campo_de_visao': 1,
+                    'campo_de_visao': 3,
                     'movimentacao': True,
                     'direita_movimentacao': True
                 },
@@ -155,6 +155,130 @@ level_data = [
             {
                 'A': [0],
                 'T': []
+            }
+        ]
+    },
+        {
+        'level_map': [
+            'XXXXXXXXXXXXXXXXXX',
+            'X               GX',
+            'X P A            X',
+            'XXXXX         XXXX',
+            'XM    R          X',
+            'X                X',
+            'X     XXX        X',
+            'X                X',
+            'X  V             X',
+            'X            XXXXX',
+            'X        H       X',
+            'X    B    R     CX',
+            'XXXXXXXXXXXXXXXXXX'
+        ], 'sprite_settings': {
+            'R': [
+                {
+                    'x_distancia': 1,
+                    'campo_de_visao': 10,
+                    'movimentacao': False,
+                    'direita_movimentacao': False
+                },                
+                {
+                    'x_distancia': 4,
+                    'campo_de_visao': 3,
+                    'movimentacao': True,
+                    'direita_movimentacao': True
+                }
+            ], 'H': [
+                {
+                    'variacao_x': (-1, 1),
+                    'platform_vel': 0.03
+                }
+            ], 'V': [
+                {
+                    'variacao_y': (-1, 1)
+                },
+                {
+                    'variacao_y': (-1, 2)
+                }
+            ], 'M': [
+                {
+                    'variacao_x': (-1, 6),
+                    'horizontal': True
+                },
+                                {
+                    'variacao_x': (0, 6),
+                    'horizontal': True
+                }
+            ]
+        }, 'connections': [
+            {
+                'A': [0],
+                'T': []
+            }
+        ]
+    },
+    {
+        'level_map': [
+            'XXXXXXXXXXXXXXXXXX',
+            'X                X',
+            'X    G     B   P X',
+            'X   XXX   XXX  XXX',
+            'X    M           X',
+            'X R         R    X',
+            'XXXX  XXXXXXXXX  X',
+            'X               VX',
+            'X        R    C  X',
+            'XXX  XXXXX  XX   X',
+            'X                X',
+            'X   T            X',
+            'XXXXXXXXXXXXXXXXXX'
+        ], 'sprite_settings': {
+            'R': [
+                {
+                    'x_distancia': 1,
+                    'campo_de_visao': 5,
+                    'movimentacao': True,
+                    'direita_movimentacao': True
+                },
+                {
+                    'x_distancia': 5,
+                    'campo_de_visao': 5,
+                    'movimentacao': True,
+                    'direita_movimentacao': False
+                },
+                {
+                    'x_distancia': 5,
+                    'campo_de_visao': 5,
+                    'movimentacao': True,
+                    'direita_movimentacao': True
+                },
+                {
+                    'x_distancia': 5,
+                    'campo_de_visao': 5,
+                    'movimentacao': True,
+                    'direita_movimentacao': True
+                },
+            ], 'H': [
+                {
+                    'variacao_x': (-1, 1),
+                    'platform_vel': 0.03
+                }
+            ], 'V': [
+                {
+                    'variacao_y': (-1, 1)
+                },
+                {
+                    'variacao_y': (-1, 2)
+                }
+            ], 'M': [
+                {
+                    'variacao_x': (-1, 6),
+                    'horizontal': True
+                },
+            ]
+        }, 'connections': [
+            {
+                'A': [],
+                'T': [0]
             }
         ]
     }]
